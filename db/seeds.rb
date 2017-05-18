1f1d4f6e35de88b48184b3d5801d90e110dbc51f
Brandon Chui <bbchui325@gmail.com>
2017-05-18 14:45:51 -0700

    Added 3 more seeds to local database for testing, some styling changes to Featured Gyms

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@
 #   city: "San Francisco",
 #   state: "CA",
 #   zip: "94131",
-#   level: "beginner-intermediate",
+#   level: "Beginner-Intermediate",
 #   price: "Free",
 #   hours: "Wednesday: 6pm - 8:30pm, Friday: 6pm - 8:30pm",
 #   phone: "(415) 970-8061",
@@ -27,7 +27,7 @@
 #   city: "San Francisco",
 #   state: "CA",
 #   zip: "94105",
-#   level: "beginner-intermediate",
+#   level: "Beginner-Intermediate",
 #   price: "Membership Required",
 #   hours: "Wednesday: 6pm - 9pm",
 #   phone: "(415) 957-9622",
@@ -41,10 +41,55 @@
 #   city: "San Francisco",
 #   state: "CA",
 #   zip: "94121",
-#   level: "beginner-intermediate",
+#   level: "Beginner-Intermediate",
 #   price: "Free",
 #   hours: "Thursday: 6pm - 8:30pm",
 #   phone: "(415) 666-7020",
 #   website_url: "http://sfrecpark.org/destination/richmond-rec-center/",
 #   image_url: "http://sfrecpark.org/wp-content/uploads/RichmondRC.jpg"
 #   })
+
+# Gym.create!({
+#   name: "UC Davis ARC",
+#   address: "232 Shields Ave",
+#   city: "Davis",
+#   state: "CA",
+#   zip: "95616",
+#   level: "Intermediate",
+#   price: "$7 one-day pass, monthly membership available",
+#   hours: "Everyday 7pm - 11pm",
+#   phone: "(520) 752-1730",
+#   website_url: "https://cru.ucdavis.edu/content/1-activities-and-recreation-center-arc.htm
+# ",
+#   image_url: "http://admitguide.com/wp-content/uploads/2015/02/UC_Davis_Arc.jpg"
+#   })
+
+
+  # Gym.create!({
+  #   name: "Betty Ong Rec Center",
+  #   address: "1199 Mason Street",
+  #   city: "San Francisco",
+  #   state: "CA",
+  #   zip: "94108",
+  #   level: "Beginner-Intermediate",
+  #   price: "Free",
+  #   hours: "Wednesday 6:30 - 9pm",
+  #   phone: "(415) 359-9103",
+  #   website_url: "https://cru.ucdavis.edu/content/1-activities-and-recreation-center-arc.htm
+  # ",
+  #   image_url: "https://s3-media2.fl.yelpcdn.com/bphoto/FPgFP6Ajx9ED4T6xVB-Pbw/o.jpg"
+  #   })
+
+  # Gym.create!({
+  #   name: "Arrillaga Family Recreation Center",
+  #   address: "610 Laurel St",
+  #   city: "Menlo Park",
+  #   state: "CA",
+  #   zip: "94025",
+  #   level: "Intermediate-Expert",
+  #   price: "$3 a day",
+  #   hours: "Sunday 6:30 - 9:30pm",
+  #   phone: "(650) 330-2200",
+  #   website_url: "",
+  #   image_url: "https://www.menlopark.org/ImageRepository/Path?filePath=%2Fdocuments%5CIntranet%5C19%5C100%5C140%5C141%5C612%2FGymnastics6_201404171920307971.jpg"
+  #   })
--- frontend/components/gyms/gyms_index_item.jsx
+++ frontend/components/gyms/gyms_index_item.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const GymIndexItem = ({gym}) => (
       <li className="gym-info-featured">
-        <img className="featured" src={gym.image_url} alt="Not Working" />
-        <span> { gym.name } </span>
-        <span> Rating </span>
-        <span> { gym.level } </span>
-        <span> { gym.city } </span>
+        <img className="featured" src={gym.image_url} alt="Image Not Working" />
+        <Link className="featured-gym-name" to={`/gyms/${gym.id}`}>
+          { gym.name }
+        </Link>
+        <span> Rating
+         </span>
+        <span className="feature-text"> { gym.level } </span>
+        <span className="feature-text"> { gym.city } </span>
       </li>
 );
 
