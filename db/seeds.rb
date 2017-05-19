64b0d4ccf7812fc4b633f411f711a85a5f43ecb2
Brandon Chui <bbchui325@gmail.com>
2017-05-18 17:27:42 -0700

    First attempt seeding heroku db

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,89 +7,89 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-# Gym.create!({
-#   name: "Upper Noe Recreation Center",
-#   address: "295 Day St.",
-#   city: "San Francisco",
-#   state: "CA",
-#   zip: "94131",
-#   level: "Beginner-Intermediate",
-#   price: "Free",
-#   hours: "Wednesday: 6pm - 8:30pm, Friday: 6pm - 8:30pm",
-#   phone: "(415) 970-8061",
-#   website_url: "www.noevalleyreccenter.com",
-#   image_url: "https://lh5.googleusercontent.com/-JKLzU8vW-TE/TXFWzCk_mfI/AAAAAAAAAEM/V9Ozka9bbTA/s1600/noevallyreccenter.jpg"
-#   })
+Gym.create!({
+  name: "Upper Noe Recreation Center",
+  address: "295 Day St.",
+  city: "San Francisco",
+  state: "CA",
+  zip: "94131",
+  level: "Beginner-Intermediate",
+  price: "Free",
+  hours: "Wednesday: 6pm - 8:30pm, Friday: 6pm - 8:30pm",
+  phone: "(415) 970-8061",
+  website_url: "www.noevalleyreccenter.com",
+  image_url: "https://lh5.googleusercontent.com/-JKLzU8vW-TE/TXFWzCk_mfI/AAAAAAAAAEM/V9Ozka9bbTA/s1600/noevallyreccenter.jpg"
+  })
 
-# Gym.create!({
-#   name: "Embarcadero YMCA",
-#   address: "169 Steuart St.",
-#   city: "San Francisco",
-#   state: "CA",
-#   zip: "94105",
-#   level: "Beginner-Intermediate",
-#   price: "Membership Required",
-#   hours: "Wednesday: 6pm - 9pm",
-#   phone: "(415) 957-9622",
-#   website_url: "https://www.ymcasf.org/locations/embarcadero-ymca",
-#   image_url: "https://www.gymaround.com/wp-content/uploads/edd/2015/03/YMCA-Embarcadero.jpg"
-#   })
+Gym.create!({
+  name: "Embarcadero YMCA",
+  address: "169 Steuart St.",
+  city: "San Francisco",
+  state: "CA",
+  zip: "94105",
+  level: "Beginner-Intermediate",
+  price: "Membership Required",
+  hours: "Wednesday: 6pm - 9pm",
+  phone: "(415) 957-9622",
+  website_url: "https://www.ymcasf.org/locations/embarcadero-ymca",
+  image_url: "https://www.gymaround.com/wp-content/uploads/edd/2015/03/YMCA-Embarcadero.jpg"
+  })
 
-# Gym.create!({
-#   name: "Richmond Recreation Center",
-#   address: "251 18th Ave",
-#   city: "San Francisco",
-#   state: "CA",
-#   zip: "94121",
-#   level: "Beginner-Intermediate",
-#   price: "Free",
-#   hours: "Thursday: 6pm - 8:30pm",
-#   phone: "(415) 666-7020",
-#   website_url: "http://sfrecpark.org/destination/richmond-rec-center/",
-#   image_url: "http://sfrecpark.org/wp-content/uploads/RichmondRC.jpg"
-#   })
+Gym.create!({
+  name: "Richmond Recreation Center",
+  address: "251 18th Ave",
+  city: "San Francisco",
+  state: "CA",
+  zip: "94121",
+  level: "Beginner-Intermediate",
+  price: "Free",
+  hours: "Thursday: 6pm - 8:30pm",
+  phone: "(415) 666-7020",
+  website_url: "http://sfrecpark.org/destination/richmond-rec-center/",
+  image_url: "http://sfrecpark.org/wp-content/uploads/RichmondRC.jpg"
+  })
 
-# Gym.create!({
-#   name: "UC Davis ARC",
-#   address: "232 Shields Ave",
-#   city: "Davis",
-#   state: "CA",
-#   zip: "95616",
-#   level: "Intermediate",
-#   price: "$7 one-day pass, monthly membership available",
-#   hours: "Everyday 7pm - 11pm",
-#   phone: "(520) 752-1730",
-#   website_url: "https://cru.ucdavis.edu/content/1-activities-and-recreation-center-arc.htm
-# ",
-#   image_url: "http://admitguide.com/wp-content/uploads/2015/02/UC_Davis_Arc.jpg"
-#   })
+Gym.create!({
+  name: "UC Davis ARC",
+  address: "232 Shields Ave",
+  city: "Davis",
+  state: "CA",
+  zip: "95616",
+  level: "Intermediate",
+  price: "$7 one-day pass, monthly membership available",
+  hours: "Everyday 7pm - 11pm",
+  phone: "(520) 752-1730",
+  website_url: "https://cru.ucdavis.edu/content/1-activities-and-recreation-center-arc.htm
+",
+  image_url: "http://admitguide.com/wp-content/uploads/2015/02/UC_Davis_Arc.jpg"
+  })
 
 
-  # Gym.create!({
-  #   name: "Betty Ong Rec Center",
-  #   address: "1199 Mason Street",
-  #   city: "San Francisco",
-  #   state: "CA",
-  #   zip: "94108",
-  #   level: "Beginner-Intermediate",
-  #   price: "Free",
-  #   hours: "Wednesday 6:30 - 9pm",
-  #   phone: "(415) 359-9103",
-  #   website_url: "https://cru.ucdavis.edu/content/1-activities-and-recreation-center-arc.htm
-  # ",
-  #   image_url: "https://s3-media2.fl.yelpcdn.com/bphoto/FPgFP6Ajx9ED4T6xVB-Pbw/o.jpg"
-  #   })
+  Gym.create!({
+    name: "Betty Ong Rec Center",
+    address: "1199 Mason Street",
+    city: "San Francisco",
+    state: "CA",
+    zip: "94108",
+    level: "Beginner-Intermediate",
+    price: "Free",
+    hours: "Wednesday 6:30 - 9pm",
+    phone: "(415) 359-9103",
+    website_url: "https://cru.ucdavis.edu/content/1-activities-and-recreation-center-arc.htm
+  ",
+    image_url: "https://s3-media2.fl.yelpcdn.com/bphoto/FPgFP6Ajx9ED4T6xVB-Pbw/o.jpg"
+    })
 
-  # Gym.create!({
-  #   name: "Arrillaga Family Recreation Center",
-  #   address: "610 Laurel St",
-  #   city: "Menlo Park",
-  #   state: "CA",
-  #   zip: "94025",
-  #   level: "Intermediate-Expert",
-  #   price: "$3 a day",
-  #   hours: "Sunday 6:30 - 9:30pm",
-  #   phone: "(650) 330-2200",
-  #   website_url: "",
-  #   image_url: "https://www.menlopark.org/ImageRepository/Path?filePath=%2Fdocuments%5CIntranet%5C19%5C100%5C140%5C141%5C612%2FGymnastics6_201404171920307971.jpg"
-  #   })
+  Gym.create!({
+    name: "Arrillaga Family Recreation Center",
+    address: "610 Laurel St",
+    city: "Menlo Park",
+    state: "CA",
+    zip: "94025",
+    level: "Intermediate-Expert",
+    price: "$3 a day",
+    hours: "Sunday 6:30 - 9:30pm",
+    phone: "(650) 330-2200",
+    website_url: "",
+    image_url: "https://www.menlopark.org/ImageRepository/Path?filePath=%2Fdocuments%5CIntranet%5C19%5C100%5C140%5C141%5C612%2FGymnastics6_201404171920307971.jpg"
+    })
