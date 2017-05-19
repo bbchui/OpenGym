5094f96191e3c8c076d2caa7552ea6b366e01a37
Brandon Chui <bbchui325@gmail.com>
2017-05-18 22:20:32 -0700

    Fixed styling on search index page

--- frontend/components/gyms/gyms_search_item.jsx
+++ frontend/components/gyms/gyms_search_item.jsx
@@ -2,15 +2,29 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const GymSearchItem = ({gym}) => (
-  <li className="gym-info-featured">
-    <img className="featured" src={gym.image_url} alt="Image Not Working" />
-    <Link className="featured-gym-name" to={`/gyms/${gym.id}`}>
-      { gym.name }
-    </Link>
-    <span> Rating
-     </span>
-    <span className="feature-text"> { gym.level } </span>
-    <span className="feature-text"> { gym.city } </span>
+  <li className="gym-info-searched">
+    <img className="searched" src={gym.image_url} alt="Image Not Working" />
+
+    <div className="gym-index-info">
+      <div>
+        <Link className="searched-gym-name" to={`/gyms/${gym.id}`}>
+          { gym.name }
+        </Link>
+        <span> Rating *****
+         </span>
+         <span> { gym.price } </span>
+      </div>
+
+      <div>
+        <span className="feature-text">
+          { gym.address }
+          <br/>
+        { gym.city }, { gym.state } { gym.zip }
+         </span>
+        <span className="feature-text"> { gym.phone } </span>
+        <span className="feature-text"> { gym.website} </span>
+        </div>
+    </div>
   </li>
 )
 
