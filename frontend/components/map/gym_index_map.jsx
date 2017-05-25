a5499ffe8be8906c68def816f9461b325c65a0b1
Brandon Chui <bbchui325@gmail.com>
2017-05-24 18:15:15 -0700

    working on map zoom

--- frontend/components/map/gym_index_map.jsx
+++ frontend/components/map/gym_index_map.jsx
@@ -5,21 +5,61 @@ import MarkerManager from '../../util/marker_manager';
 
 class GymIndexMap extends React.Component {
 
-
   componentDidMount() {
+    let center;
+    if (this.props.gyms[0]) {
+      center = { lat: this.props.gyms[0].lat,
+                  lng: this.props.gyms[0].lng}
+    } else {
+      center = { lat: 37.7758, lng: -122.435 }
+    }
+
+
     const mapOptions = {
-      center: { lat: 37.7758, lng: -122.435 },
-      zoom: 11
+      center: center,
+      zoom: 10
     };
 
-    // wrap the mapDOMNode in a Google Map
     this.map = new google.maps.Map(this.mapNode, mapOptions);
     this.MarkerManager = new MarkerManager(this.map);
     this.MarkerManager.updateMarkers(this.props.gyms)
   }
 
   componentWillReceiveProps(nextProps) {
+    let center;
+    let zoom;
+    console.log(nextProps);
+    if (Object.keys(nextProps.gyms).length === 1) {
+      center = { lat: nextProps.gyms[0].lat,
+                  lng: nextProps.gyms[0].lng};
+      zoom = 14;
+    } else if (nextProps.gyms[0]) {
+      center = { lat: nextProps.gyms[0].lat,
+                  lng: nextProps.gyms[0].lng}
+      zoom = 11;
+    } else {
+      center = { lat: 37.7758, lng: -122.435 }
+      zoom = 10;
+    }
+
+    const mapOptions = {
+      center: center,
+      zoom: zoom
+    };
+
+    this.map = new google.maps.Map(this.mapNode, mapOptions);
+    this.MarkerManager = new MarkerManager(this.map)
     this.MarkerManager.updateMarkers(nextProps.gyms);
+
+    const markers = this.MarkerManager.markers;
+    // console.log(markers);
+  }
+
+  gymInfo(marker) {
+    let gymId = marker.gymId;
+    let gym = this.props.gyms[gymId];
+
+
   }
 
   render() {
