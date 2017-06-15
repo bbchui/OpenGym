311b5b4ab1817e156bb0bf9e7c1cc5be424c3689
bbchui <%= GitConfig.user_email %>
2017-06-14 23:01:44 -0700

    testing google autocomplete

--- frontend/components/search/test_search.jsx
+++ frontend/components/search/test_search.jsx
@@ -8,6 +8,7 @@ class TestSearch extends React.Component {
     this.handleAutocomplete = this.handleAutocomplete.bind(this);
     this.handleLocationInput = this.handleLocationInput.bind(this);
     this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    this.update = this.update.bind(this);
   }
 
   componentDidMount() {
@@ -16,12 +17,11 @@ class TestSearch extends React.Component {
       const place = autocomplete.getPlace().name;
       this.handleAutocomplete(place);
     });
-    // google.maps.event.addDomListener(document.getElementById('txtPlaces'), 'keydown', function(e) {
-    //   if (e.keyCode === 13) {
-    //     e.preventDefault();
-    //   }
-    // });
-    this.props.getAllGyms(this.state.query)
+    google.maps.event.addDomListener(document.getElementById('txtPlaces'), 'keydown', function(e) {
+      if (e.keyCode === 13) {
+        e.preventDefault();
+      }
+    });
   }
 
   handleAutocomplete(place) {
@@ -29,21 +29,41 @@ class TestSearch extends React.Component {
   }
   handleLocationInput(event) {
     this.setState({query: event.currentTarget.value.split(',')[0]});
+    console.log(this.state.query);
   }
 
-  handleFormSubmit(event) {
-    // event.preventDefault();
+  handleFormSubmit(e) {
+    // e.preventDefault();
+    // console.log(e.keycCode, this.state.query, "here");
     this.props.getAllGyms(this.state.query)
-    .then(this.props.history.push(`/gyms`));
-    // this.clearFields();
-    this.setState({city: ''});
+      .then(() => {
+        if (this.props.location.hash !== "/gyms") {
+          this.props.history.push('/gyms')
+        }
+      });
   }
 
+  homeRender() {
+    if (this.props.location.pathname === "/") {
+      return "home-search"
+    } else {
+      return "else-search"
+    }
+  }
+
+  update(e) {
+    this.setState({ query: e.currentTarget.value });
+  }
 
   render() {
     return (
-      <form onSubmit={this.handleFormSubmit} className="else-search">
-        <input onBlur={this.handleLocationInput} type="text" id="txtPlaces" placeholder="Here" />
+      <form onSubmit={this.handleFormSubmit} className={this.homeRender()}>
+        <input onChange={this.update}
+          onBlur={this.handleLocationInput}
+          type="text"
+          id="txtPlaces"
+          placeholder="Search by City"
+          className={this.homeRender()} />
         <button>
           <i className="fa fa-search fa-2x" aria-hidden="true"></i>
         </button>
